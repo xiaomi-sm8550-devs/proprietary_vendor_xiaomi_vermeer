8e87a5a5e058047132241626f5d78c5df09a2e0f
ByteWave1014 <jvillaltamatos3007@gmail.com>
2025-10-16 10:36:38 -0500

    vermeer: Import blobs & firmware from OS2.0.206.0.VNKMIXM
    Change-Id: I48bb1c3737ad6d478be75be17bf43da5c8d1e171
    Signed-off-by: Lunark :3 <jvillaltamatos3007@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,30 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),vermeer)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,63f81ed6de7f81c76dd57aa10ab95a25a4d7eafb)
+$(call add-radio-file-sha1-checked,radio/aop.img,adb87009977f8e1c98982077f008b87a7961cd35)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,965efbb7c118887ef70cb15704f87edcda6fd365)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,93c5956aaf005880a5083410ceaa92fa39e9fa4a)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,c13dbbdb9074281b677fba054fb35f485e3f0c40)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,a07ff10ae5eb7af4be53a5e7531639b4ffaca62b)
+$(call add-radio-file-sha1-checked,radio/dsp.img,bf0dcc0e03350003f6119058ce1737bddc4b333a)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,aa339db5c9214aad087466c649a2805ab1cffad9)
+$(call add-radio-file-sha1-checked,radio/hyp.img,c16af6eb8511a93112a2531e3b017b722073f93d)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,54638cbbed80f828ffae234d6d8fc51fb007eb18)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,451e2f4ff3a08894396399254e2a533db49bb432)
+$(call add-radio-file-sha1-checked,radio/modem.img,452a3f87cfc04dd3a0253e1e1ee9330a2e8af3bb)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,2a4b3cfbe7968c362085feeed6f073844c74521e)
+$(call add-radio-file-sha1-checked,radio/shrm.img,96233679664bf7d9e31f5c3987324b0bcfb1770b)
+$(call add-radio-file-sha1-checked,radio/tz.img,1da3d4c6b1c19218004943b75866a93cc4935d95)
+$(call add-radio-file-sha1-checked,radio/uefi.img,a07758a7d58d8686f0e6576e1fe420ef53740add)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,db9644eec4b87b1576adbde754858b1841c981a7)
+$(call add-radio-file-sha1-checked,radio/xbl.img,aae23b1f8bb3e090a386eedccdeb7493fb7ebad3)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,990784c8be5b8190df3d953f98d5fee719a9f59d)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,13fb999a76dd09fa9e91c9c392e674b0d6bdb168)
+
+endif
